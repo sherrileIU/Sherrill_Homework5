464b90e11b9e7e1908dfbf098a1a8921ca224c8e
sherrileIU <sherrile@indiana.edu>
2020-10-19 20:07:48 -0400

    sweep speed test

--- llr_functions.R
+++ llr_functions.R
@@ -23,7 +23,7 @@ llr <- function(x, y, z, omega) {
 compute_f_hat <- function(z, x, y, omega) {
   Wz <- diag(make_weight_matrix(z, x, omega))
   X <- make_predictor_matrix(x)
-  f_hat <- c(1, z) %*% solve(t(X) %*% apply(X, 2, function(x) x*Wz)) %*% t(X) %*% (Wz * y)
+  f_hat <- c(1, z) %*% solve(t(X) %*% sweep(X, 1, Wz, "*")) %*% t(X) %*% (Wz * y)
   return(f_hat)
 }
 
